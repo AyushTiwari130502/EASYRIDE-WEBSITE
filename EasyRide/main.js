a3e5105e038baa5b214f8359ec1f07efba49de01
AyushTiwari130502 <108716055+AyushTiwari130502@users.noreply.github.com>
2023-07-26 19:21:16 +0530

    Add files via upload

---
+++ EasyRide/main.js
@@ -0,0 +1,43 @@
+const mobileNav = document.querySelector(".btn-mobile-nav");
+const headerMain = document.querySelector("#header-section");
+
+mobileNav.addEventListener("click", function () {
+  console.log("hello");
+  headerMain.classList.toggle("nav-open");
+  if (headerMain.classList.contains("nav-open")) {
+    document.documentElement.style.overflowY = "hidden";
+  } else {
+    document.documentElement.style.overflowY = "visible";
+  }
+});
+
+const navLinks = document.querySelectorAll(".main-nav-link");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    if (headerMain.classList.contains("nav-open")) {
+      headerMain.classList.remove("nav-open");
+      document.documentElement.style.overflowY = "visible";
+    }
+  });
+});
+
+// Sticky menu
+const sectHero = document.querySelector("#section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      headerMain.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      headerMain.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-64px",
+  }
+);
+observer.observe(sectHero);
---
+++ EasyRide/main1.js
@@ -0,0 +1,18 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxl4nqRlIThRDUSe0gtIgNWkS-owhqa5TBvoM-4XrOF6xSHV-WPiRkWEu1zJXNGVeIO/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg=document.getElementById('msg')
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          msg.innerHTML =" Thank you for your booking!"
+          setTimeout(function(){
+            msg.innerHTML =" "
+
+          },5000)
+          form.reset()
+        
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
